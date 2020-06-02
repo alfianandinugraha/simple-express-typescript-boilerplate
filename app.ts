1867260826299e3f2957e13a61574ff5e23c007d
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-06-02 14:05:14 +0700

    add app.ts file and config for express

---
+++ app.ts
@@ -0,0 +1,46 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+import bodyParser from 'body-parser'
+
+class App{
+    public app: Application
+    public port: number
+
+    constructor(port: number) {
+        this.port = port
+        this.app = express()
+        this.config()
+        this.routes()
+    }
+
+    // config express
+    private config(): void{
+        const { app } = this
+
+        app.use(bodyParser.urlencoded({ extended: false }))
+        app.use(express.json())
+    }
+
+    // contains all routes
+    private routes(): void {
+        const { app } = this
+
+        app.get('/', (req: Request, res: Response) => {
+            
+        })
+
+        app.post('/', (req: Request, res: Response) => {
+            
+        })
+    }
+
+    run() {
+        let messageListen = () => {
+            console.log(`Server is running at http://localhost:${this.port}`)
+        }
+
+        this.app.listen(this.port, messageListen)
+    }
+}
+
+const PORT = 3000
+new App(PORT).run()
