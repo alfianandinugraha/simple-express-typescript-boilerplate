e51e5626d9114ed1d90cce660fa9d1672a1357c1
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-06-02 15:46:33 +0700

    wrap all to src folder

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ class App{
         app.use(express.static('public'))
 
         app.set('view engine', 'pug')
-        app.set('views', 'views')
+        app.set('views', 'src/views')
     }
 
     // add express middleware
